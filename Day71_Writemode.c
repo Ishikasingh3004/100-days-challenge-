28c4eb3d39f0304f00ff49ce449d9f7f47826a89
Ishika <Ishika.28031@stu.upes.ac.in>
2025-11-29 13:40:07 +0530

    Create Day71_Writemode.c

---
+++ Day71_Writemode.c
@@ -0,0 +1,34 @@
+//Q121: Write a C program that creates a text file named info.txt in write mode. The program should take the user’s name and age as input, and write them to the file using fprintf(). After writing, display a message confirming that the data was successfully saved.
+#include <stdio.h>
+#include <ctype.h>  
+void toSentenceCase(char str[]) {
+    int capitalize = 1;  
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (capitalize && isalpha(str[i])) {
+            str[i] = toupper(str[i]); 
+            capitalize = 0;
+        } else {
+            str[i] = tolower(str[i]);  
+        }
+
+        if (str[i] == ' ') {
+            capitalize = 1; 
+        }
+    }
+}
+
+int main() {
+    char str1[] = "I am trying to build logic.";
+    toSentenceCase(str1);
+    printf("%s\n", str1);  
+    char str2[] = "The classes are supposed to start early.";
+    toSentenceCase(str2);
+    printf("%s\n", str2);  
+
+    char str3[] = "We are going to look at 26 different test cases.";
+    toSentenceCase(str3);
+    printf("%s\n", str3);  
+
+    return 0;
+}
