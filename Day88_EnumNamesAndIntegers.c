50f924ca591cbaf734f595a20f8bb42ad815b12a
Ishika <Ishika.28031@stu.upes.ac.in>
2025-11-29 14:32:43 +0530

    Add loop to print enum names and values
    Implemented a loop to print enum names and their integer values.

---
+++ Day88_EnumNamesAndIntegers.c
@@ -0,0 +1,10 @@
+//Q138: Print all enum names and integer values using a loop.
+#include <stdio.h>
+enum Colors { RED, YELLOW, GREEN };
+int main() {
+    const char *colorNames[] = { "RED", "YELLOW", "GREEN" };
+    for (int i = RED; i <= GREEN; i++) {
+        printf("%s=%d\n", colorNames[i], i);
+    }
+    return 0;
+}
